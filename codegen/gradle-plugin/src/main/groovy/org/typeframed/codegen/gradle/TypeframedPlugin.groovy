466b7f93c937af94b86c37c760a902fa6b7d489e
Lars J. Nilsson <contact@larsan.net>
2015-04-10 19:02:42 +0200

    updating license headers to 2015

--- codegen/gradle-plugin/src/main/groovy/org/typeframed/codegen/gradle/TypeframedPlugin.groovy
+++ codegen/gradle-plugin/src/main/groovy/org/typeframed/codegen/gradle/TypeframedPlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Lars J. Nilsson <contact@larsan.net>
+ * Copyright 2015 Lars J. Nilsson <contact@larsan.net>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
